7644bd03a8646d4eef707f29f82bfd17b737c676
scratchor <scratchor@mail.ru>
2019-05-20 16:03:44 +0300

    add bootstrap nav

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -11,44 +11,60 @@ import {
 
 import styled from 'styled-components';
 
-const Styles = styled.div`
-  a.active {
+const Wrapper = styled.div`
+  .mr-auto > .nav-link.active {
     color: white;
+  }
+
+  .login {
+  }
+
+  .form-control {
+    margin: 10px 0;
   }
 `;
 
-export const NavBar = () => {
-  return (
-    <Styles>
-      <Navbar bg="info" expand="md">
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <LinkContainer exact to="/">
-              <Nav.Link>Home</Nav.Link>
+export const NavBar = () => (
+  <Wrapper>
+    <Navbar bg="info" expand="lg">
+      <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+        <Nav className="ml-auto">
+          <LinkContainer exact to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer exact to="/about">
+            <Nav.Link>About</Nav.Link>
+          </LinkContainer>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <LinkContainer exact to="/action/3.1">
+              <NavDropdown.Item>Action</NavDropdown.Item>
             </LinkContainer>
-            <LinkContainer exact to="/about">
-              <Nav.Link>About</Nav.Link>
-            </LinkContainer>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-    </Styles>
-  );
-};
+            <NavDropdown.Item href="action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <LinkContainer exact to="/login">
+            <Nav.Link className="ml-0 ml-lg-4">
+              <Button className="login" variant="dark">
+                Login
+              </Button>
+            </Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Wrapper>
+);
